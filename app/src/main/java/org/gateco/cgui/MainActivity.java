e211ee678ec2ec28d9ad9d6040c601a71a4e6779
gateVahid <vahid_fattahzadeh@yahoo.com>
2017-08-12 17:38:09 +0430

    gfjmfgn

--- app/src/main/java/org/gateco/cgui/MainActivity.java
+++ app/src/main/java/org/gateco/cgui/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cg_catview);
+        setContentView(R.layout.cg_schedule);
     }
 }
